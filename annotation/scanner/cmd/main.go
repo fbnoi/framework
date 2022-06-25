5759cefeebad9c836a6e7e036c8e6c685b188c7d
fbnoi <fbnoi@foxmail.com>
2022-06-25 22:25:02 +0800

    Update scanner

--- annotation/scanner/cmd/main.go
+++ annotation/scanner/cmd/main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
+	"fbnoi.com/framework/annotation/scanner"
 )
 
 func main() {
-	fSet := token.NewFileSet()
-	f, err := parser.ParseFile(fSet, "../../../example/controller/controller1.go", nil, parser.ParseComments)
-	if err != nil {
-		panic(err)
-	}
-
-	ast.Inspect(f, func(n ast.Node) bool {
-		switch t := n.(type) {
-		case *ast.FuncDecl:
-			if t.Doc != nil {
-				for _, c := range t.Doc.List {
-					fmt.Println(c.Text, "PP")
-				}
-				fmt.Println(t.Name.String())
-			}
-		}
-		return true
-	})
+	scanner.Scan("./example/controller/controller1.go")
 }
--- annotation/scanner/scanner.go
+++ annotation/scanner/scanner.go
@@ -1,7 +1,6 @@
 package scanner
 
 import (
-	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -16,35 +15,5 @@ func Scan(path string) {
 	if err != nil {
 		panic(err)
 	}
-
-	ast.Inspect(f, func(n ast.Node) bool {
-		switch t := n.(type) {
-		case *ast.FuncDecl:
-			if t.Doc != nil {
-				fmt.Println(t.Doc.Text())
-				fmt.Print(t.Name.String())
-				typ := t.Type
-				if typ.Params != nil {
-					for _, p := range typ.Params.List {
-						for _, o := range p.Names {
-							fmt.Print(o.Name, " ")
-						}
-						typ := p.Type
-						if t, ok := typ.(*ast.StarExpr); ok {
-							fmt.Print(t.X, " ")
-						}
-					}
-				}
-
-			}
-		case *ast.TypeSpec:
-			fmt.Println(t.Doc.Text())
-		case *ast.StructType:
-			for _, field := range t.Fields.List {
-				fmt.Println(field.Doc.Text())
-				fmt.Println(field.Comment.Text())
-			}
-		}
-		return true
-	})
+	ast.Print(fSet, f)
 }
